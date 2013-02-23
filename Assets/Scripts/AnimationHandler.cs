73f4ce3443823c66a2572ae10d77ed6aa8b3e986
Héctor Candón Arenas <hecktor45@gmail.com>
2013-02-24 00:16:00 +0100

    Animation Skeleton Version Update - v1.0
    Major Changes: Finished the Animation Handler Script
    Minor Changes: Added all the animation steps.

--- Assets/Scripts/AnimationHandler.cs
+++ Assets/Scripts/AnimationHandler.cs
@@ -3,17 +3,30 @@ using System.Collections;
 
 public class AnimationHandler : MonoBehaviour {
 	
-	public Texture2D[] idleAnimation;
-	public Texture2D[] runAnimation;
-	int m_step = 0;
-	float m_stepStatus = 0;
-	float m_animationSpeed = 0;
-	Movement.PlayerState m_actualState;	
+	public Texture2D[] airAnimation;
+	public Texture2D[] attackAnimation;	
+	public Texture2D[] hurtAnimation;
+	public Texture2D[] idleAnimation;	
+	public Texture2D[] jumpAnimation;
+	public Texture2D[] runAnimation;	
+	public Texture2D[] slideAnimation;
+	public Texture2D[] wallAnimation;
+	
+	private int m_step = 0;
+	private float m_stepStatus = 0;
+	private float m_animationSpeed = 0;
+	private Movement.PlayerState m_actualState;	
+	private int m_direction = -1;
 	
 	public void setAnimation (Movement.PlayerState newState, float animationSpeed) {
 		m_actualState = newState;
 		m_step = 0;
+		m_stepStatus = 0;
 		m_animationSpeed = animationSpeed;
+	}
+	
+	private void setDirection(int dir) {
+		m_direction = dir;
 	}
 	
 	// Use this for initialization
@@ -23,19 +36,69 @@ public class AnimationHandler : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) setAnimation(Movement.PlayerState.Jump, 0.1f);
+		m_stepStatus = m_stepStatus + Time.deltaTime;
 		switch (m_actualState) {
+
+		case Movement.PlayerState.Air:
+			if(m_stepStatus >= m_animationSpeed) {
+				m_stepStatus %= m_animationSpeed;
+				m_step = (++m_step)%airAnimation.Length;
+			} 
+			renderer.material.mainTexture = airAnimation[m_step];
+			break;
+		case Movement.PlayerState.Attack:
+			if(m_stepStatus >= m_animationSpeed) {
+				m_stepStatus %= m_animationSpeed;
+				m_step = (++m_step)%attackAnimation.Length;
+			} 
+			renderer.material.mainTexture = attackAnimation[m_step];
+			break;
+		case Movement.PlayerState.Hurt:
+			if(m_stepStatus >= m_animationSpeed) {
+				m_stepStatus %= m_animationSpeed;
+				m_step = (++m_step)%hurtAnimation.Length;
+			} 
+			renderer.material.mainTexture = hurtAnimation[m_step];
+			break;
 		case Movement.PlayerState.Idle:
-			m_stepStatus = m_stepStatus + Time.deltaTime;
 			if(m_stepStatus >= m_animationSpeed) {
 				m_stepStatus %= m_animationSpeed;
 				m_step = (++m_step)%idleAnimation.Length;
 			} 
 			renderer.material.mainTexture = idleAnimation[m_step];
 			break;
+		case Movement.PlayerState.Jump:
+			if(m_stepStatus >= m_animationSpeed) {
+				m_stepStatus %= m_animationSpeed;
+				m_step = (++m_step)%jumpAnimation.Length;
+			} 
+			renderer.material.mainTexture = jumpAnimation[m_step];
+			break;
 		case Movement.PlayerState.Run:
+			if(m_stepStatus >= m_animationSpeed) {
+				m_stepStatus %= m_animationSpeed;
+				m_step = (++m_step)%runAnimation.Length;
+			} 
+			renderer.material.mainTexture = runAnimation[m_step];
+			break;
+		case Movement.PlayerState.Slide:
+			if(m_stepStatus >= m_animationSpeed) {
+				m_stepStatus %= m_animationSpeed;
+				m_step = (++m_step)%slideAnimation.Length;
+			} 
+			renderer.material.mainTexture = slideAnimation[m_step];
+			break;
+		case Movement.PlayerState.Wall:
+			if(m_stepStatus >= m_animationSpeed) {
+				m_stepStatus %= m_animationSpeed;
+				m_step = (++m_step)%wallAnimation.Length;
+			} 
+			renderer.material.mainTexture = wallAnimation[m_step];
 			break;
 		default:
 			break;
 		}
-	}
+		renderer.material.mainTextureScale = new Vector2((float)m_direction, 1f);
+	}	
 }
