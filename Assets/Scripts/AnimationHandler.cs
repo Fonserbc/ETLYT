656369d72fde5598efae4c2216aec2f37a90d51d
Héctor Candón Arenas <hecktor45@gmail.com>
2013-02-23 22:17:22 +0100

    Animation Skeleton v0.3
    Major Changes: Added delta time to the update script of the animations.
    Now it should work correctly with the class "Movement".
    Minor Changes: -

--- Assets/Scripts/AnimationHandler.cs
+++ Assets/Scripts/AnimationHandler.cs
@@ -6,14 +6,14 @@ public class AnimationHandler : MonoBehaviour {
 	public Texture2D[] idleAnimation;
 	public Texture2D[] runAnimation;
 	int m_step = 0;
-	int m_stepStatus = 0;
-	int m_animationSpeed = 0;
+	float m_stepStatus = 0;
+	float m_animationSpeed = 0;
 	Movement.PlayerState m_actualState;	
 	
-	void setAnimation (Movement.PlayerState newState) {
+	void setAnimation (Movement.PlayerState newState, float animationSpeed) {
 		m_actualState = newState;
 		m_step = 0;
-		//m_speed = m_animationSpeed;
+		m_animationSpeed = animationSpeed;
 	}
 	
 	// Use this for initialization
@@ -25,10 +25,9 @@ public class AnimationHandler : MonoBehaviour {
 	void Update () {
 		switch (m_actualState) {
 		case Movement.PlayerState.Idle:
-			m_stepStatus = m_stepStatus + m_animationSpeed;
-
-			if(m_stepStatus >= 100) {
-				m_stepStatus %= 100;
+			m_stepStatus = m_stepStatus + Time.deltaTime;
+			if(m_stepStatus >= m_animationSpeed) {
+				m_stepStatus %= m_animationSpeed;
 				m_step = (++m_step)%idleAnimation.Length;
 			} 
 			renderer.material.mainTexture = idleAnimation[m_step];
