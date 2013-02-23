2f5df1d9c2f842cbcc17035c60f98d931092461f
Héctor Candón Arenas <hecktor45@gmail.com>
2013-02-23 21:08:37 +0100

    Animation Skeleton v.0.2
    Minor Changes: Saved the script
    Major Changes: -

--- Assets/Scripts/AnimationHandler.cs
+++ Assets/Scripts/AnimationHandler.cs
@@ -5,8 +5,16 @@ public class AnimationHandler : MonoBehaviour {
 	
 	public Texture2D[] idleAnimation;
 	public Texture2D[] runAnimation;
-	int i = 0;
+	int m_step = 0;
+	int m_stepStatus = 0;
+	int m_animationSpeed = 0;
+//	enum animation = idle;	
 	
+	void setAnimation () {
+//		m_animation = animation
+		m_step = 0;
+		m_speed = animationSpeed;
+	}
 	
 	// Use this for initialization
 	void Start () {
@@ -15,7 +23,20 @@ public class AnimationHandler : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		i = (i+1)%(idleAnimation.Length*10);
-		renderer.material.mainTexture = idleAnimation[i/10];
+		switch (animation) {
+		case idle:
+			m_stepStatus = m_stepStatus + m_animationSpeed;
+
+			if(m_stepStatus >= 100) {
+				m_stepStatus %= 100;
+				m_step = (++m_step)%idleAnimation.Length;
+			} 
+			renderer.material.mainTexture = idleAnimation[m_step];
+			break;
+		case run:
+			break;
+		default:
+			break;
+		}
 	}
 }
