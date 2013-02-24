ef3c316ec9d2092c0fd98a39fe23782f7679d4ae
Nicolás Delgado <nicobito@gmail.com>
2013-02-24 01:38:56 +0100

    Character select
    nidsanosd

--- Assets/Scripts/Scripts Test CharacterSelect/MenuMovement.cs
+++ Assets/Scripts/Scripts Test CharacterSelect/MenuMovement.cs
@@ -7,7 +7,8 @@ public class MenuMovement : MonoBehaviour {
 	private Transform camara;
 	private int player;
 	public float acceleration = 5f;
-
+	private GameObject selection = null;
+	private GameObject selected = null;
 	// Use this for initialization
 	void Start () {
 		camara = GameObject.FindGameObjectWithTag("MainCamera").transform;
@@ -20,5 +21,31 @@ public class MenuMovement : MonoBehaviour {
 	void Update () {
 		rigidbody.MovePosition(transform.position+control.HorizontalAxis(player)*camara.right*acceleration*Time.deltaTime+control.VerticalAxis(player)*camara.up*acceleration*Time.deltaTime);
 		
+		if(control.Pause(player)) {
+			Application.LoadLevel("Scene Demo");	
+		}
+		
+		if(selection) {
+			if(control.Jump(player)) {
+				if(selected) selected.renderer.material.color = Color.white;
+				selected = selection;
+				selected.renderer.material.color = Color.blue;
+			}
+		}
+	}
+	
+	void OnTriggerEnter(Collider c) {
+		if (c.gameObject.tag == "Character") {
+			selection = c.gameObject;
+			c.renderer.material.color = Color.red;
+		}
+	}
+	
+	void OnTriggerExit(Collider c) {
+		if (c.gameObject.tag == "Character") {
+			selection = null;
+			if(selected != c.gameObject) c.renderer.material.color = Color.white;
+			else c.renderer.material.color = Color.blue;
+		}
 	}
 }
